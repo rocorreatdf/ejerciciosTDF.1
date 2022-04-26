82e687945d7052889f1ee1832330e778273b0cd1
rocorreatdf <104405078+rocorreatdf@users.noreply.github.com>
2022-04-25 23:31:44 -0300

    agregando ejercicio 1

---
+++ src/ejercicio1.ts
@@ -0,0 +1,5 @@
+let precio: number = 450.5;
+let porcentajeDelDescuento: number = 0.1;
+let descuento: number = precio * porcentajeDelDescuento;
+let precioFinal: number = precio - descuento;
+console.log(precioFinal);
