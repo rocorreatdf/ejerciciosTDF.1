081b66d866f605bf0ce30608f48a009f4710d207
rocorreatdf <104405078+rocorreatdf@users.noreply.github.com>
2022-05-06 20:07:26 -0300

    ejercicioAumentoSueldo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,26 @@
 import "./styles.css";
 
-console.log("Hola a todos");
-nnnn;
+let sueldoActual: number = Number(prompt("sueldo actual"));
+let nuevoSueldo: number = 0;
+
+switch (true) {
+  case 0 < sueldoActual && sueldoActual <= 15000:
+    nuevoSueldo = sueldoActual * 1.2;
+    console.log("Ud. tiene un aumento de 20%");
+    break;
+
+  case 15001 < sueldoActual && sueldoActual <= 20000:
+    nuevoSueldo = sueldoActual * 1.1;
+    console.log("Ud. tiene un aumento de 10%");
+    break;
+
+  case 20001 < sueldoActual && sueldoActual <= 25000:
+    nuevoSueldo = sueldoActual * 1.05;
+    console.log("Ud. tiene un aumento de 5%");
+    break;
+
+  default:
+    nuevoSueldo = sueldoActual;
+    console.log("Ud. no tiene aumento");
+    break;
+}
